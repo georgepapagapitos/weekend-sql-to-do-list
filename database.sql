15dfd64f48c3bb5a775ec0788e1108732c3c2ee6
George Papagapitos <george.g.papagapitos@gmail.com>
2021-02-13 10:41:36 -0600

    changed placeholder for input

--- database.sql
+++ database.sql
@@ -1,11 +1,11 @@
 CREATE TABLE "todos" (
 	"id" SERIAL PRIMARY KEY,
 	"todo" VARCHAR(1024) NOT NULL,
-	"isComplete" BOOLEAN DEFAULT FALSE
+	"isComplete" BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 INSERT INTO "todos" ("todo", "isComplete")
 VALUES 
-('This is your todo list', FALSE),
+('This is your to do list', FALSE),
 ('Add items', FALSE),
 ('Delete items', FALSE);
